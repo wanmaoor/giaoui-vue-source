829fa25b20b9616f9e2ea61d2f9711cb41510486
wanmaoor <wanmao1284879375@outlook.com>
2020-02-02 15:42:39 +0800

    release Button documentation

---
+++ docs/.vuepress/components/button-demo.vue
@@ -0,0 +1,16 @@
+<template>
+  <Buttons/>
+</template>
+
+<script>
+	import Buttons from "../../../src/views/Buttons"
+
+	export default {
+		name: "button-demo",
+		components: {Buttons}
+	}
+</script>
+
+<style scoped>
+
+</style>
