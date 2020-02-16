fba1d3552f021ed27620ef2bbca84a43a1eb5109
wanmaoor <wanmao1284879375@outlook.com>
2020-02-16 15:59:58 +0800

    Tooltip.vue > top placement

--- src/components/Tooltip/Tooltip.vue
+++ src/components/Tooltip/Tooltip.vue
@@ -1,15 +1,75 @@
 <template>
-  <div>tooltip</div>
+  <div
+    @mouseenter="handleToggle"
+    @mouseleave="handleToggle"
+    class="giao-container"
+  >
+    <slot/>
+    <div
+      :class="['tooltip', placement]"
+      v-show="isVisible"
+    >
+      {{content}}
+    </div>
+  </div>
 </template>
 
-<script>
-	import {Vue, Component} from "vue-property-decorator"
+<script lang="ts">
+  import {Component, Prop, Vue} from "vue-property-decorator"
 
-	@Component
-	export default class Tooltip extends Vue {
-	}
+  @Component
+  export default class Tooltip extends Vue {
+    @Prop({default: "top"}) placement!: "top" | "bottom" | "left" | "right"
+    @Prop(String) content!: string
+    isVisible = false
+
+    handleToggle() {
+      this.isVisible = !this.isVisible
+    }
+  }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .giao-container {
+    display: inline-block;
+    border: 1px solid #000;
+    position: relative;
+  }
 
+  .tooltip {
+    padding: 5px 10px;
+    background: black;
+    color: white;
+    position: absolute;
+    white-space: nowrap;
+    z-index: 10;
+    border-radius: 4px;
+
+    &::before {
+      content: '';
+      width: 0;
+      height: 0;
+      position: absolute;
+      border: 6px solid transparent;
+    }
+
+    &.top {
+      bottom: calc(100% + 10px);
+      left: 50%;
+      transform: translateX(-50%);
+
+      &::before {
+        border-top-color: #303030;
+        transform: translateX(-50%);
+        top: 100%;
+        left: 50%;
+      }
+    }
+
+    &.bottom {
+      top: calc(100% + 10px);
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
 </style>
--- src/views/Tooltips.vue
+++ src/views/Tooltips.vue
@@ -1,16 +1,51 @@
 <template>
-  <Tooltip/>
+  <div class="container">
+    <Tooltip
+      content="提示文字"
+      placement="top"
+    >
+      <Button>上</Button>
+    </Tooltip>
+    <Tooltip
+      content="提示文字"
+      placement="bottom"
+    >
+      <Button>上</Button>
+    </Tooltip>
+    <Tooltip
+      content="提示文字"
+      placement="left"
+    >
+      <Button>上</Button>
+    </Tooltip>
+    <Tooltip
+      content="提示文字"
+      placement="right"
+    >
+      <Button>上</Button>
+    </Tooltip>
+  </div>
 </template>
 
 <script>
 	import Tooltip from "../components/Tooltip/Tooltip"
+	import Button from "../components/Button/Button"
 
 	export default {
 		name: "Tooltips",
-		components: {Tooltip}
+		components: {Button, Tooltip}
 	}
 </script>
 
 <style scoped>
-
+  .container {
+    max-width: 800px;
+    margin: 30px auto;
+    border-radius: 4px;
+    box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.3);
+    padding: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 </style>
