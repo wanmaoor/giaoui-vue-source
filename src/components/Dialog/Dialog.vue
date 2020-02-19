69af3d30c20aa9d7d608457c787c62b1c9b79969
wanmaoor <wanmao1284879375@outlook.com>
2020-02-19 15:40:42 +0800

    Create Dialog.vue

---
+++ src/components/Dialog/Dialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main">
+    <div class="header">
+      提示
+      <button class="close">关闭</button>
+    </div>
+    <div class="content">
+      <slot/>
+    </div>
+    <div class="footer">
+      <button class="cancel">取消</button>
+      <button class="primary">确定</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Vue, Component} from "vue-property-decorator"
+
+  @Component
+  export default class Dialog extends Vue {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
